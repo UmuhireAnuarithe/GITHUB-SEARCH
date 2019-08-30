11b8ea6f964f1792d22b12e54646177bb79922a6
UmuhireAnuarithe <anuarithemuhire@gmail.com>
2019-08-30 14:40:03 +0200

    created component

---
+++ src/app/user-github/user-github.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-user-github',
+  templateUrl: './user-github.component.html',
+  styleUrls: ['./user-github.component.css']
+})
+export class UserGithubComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
